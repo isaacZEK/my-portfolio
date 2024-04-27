ebd1326f3c558bc2d4ca411dd76fcd3b65b9eac7
isaacZEK <isaac.chama32@gmail.com>
2024-04-27 22:06:32 +0200

    created the footer compont

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer= () =>{
+    return(
+    <footer>
+        <div>
+            <p>&copy; 2024 Isaac Chama. Alrights</p>
+        </div>
+    </footer>);
+};
