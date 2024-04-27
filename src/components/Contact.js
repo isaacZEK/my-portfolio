7b0eb0c3997827dd2642e072a8a77c09b7eabb29
isaacZEK <isaac.chama32@gmail.com>
2024-04-28 00:00:15 +0200

    move file from src to a subfolder calld in components c

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Contact = ()=>{
+    return (
+        <section id="contact">
+            <div className="container">
+                <h2>Contact</h2>
+                <p>You can reach me at: isaac.chama32@gmail.com</p>
+                <p>+260967605256</p>
+                <p>reach me on github: isaacZek</p>
+            </div>
+        </section>
+    );
+}
+
+export default Contact;
